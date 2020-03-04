2fde578456603e658838455fdcdeaef4877416af
gabrieli_coli <gabrielicolisi@hotmail.com>
2020-03-04 20:32:46 -0300

    resolucao poblema 4

--- src/br/fai/projeto04/Main.java
+++ src/br/fai/projeto04/Main.java
@@ -1,10 +1,44 @@
 package br.fai.projeto04;
 
+import java.util.concurrent.ForkJoinPool;
+
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Main app = new Main();
+		app.start();
 
+	}
+
+	private void start() {
+		usandoWhile();
+		usandoDoWhile();
+		usandoFor ();
+	}
+	private void usandoWhile() {
+		System.out.println("Usando While");
+		int i=0;
+		while(i< 20) {
+			System.out.println("i:" +i);
+		}
+	}
+	
+	private void usandoDoWhile() {
+		System.out.println("Usando Do While");
+		int i=0;
+		do {
+			System.out.println("k: " +i);
+		}while(i<20);
+	}
+	private void usandoFor () {
+		System.out.println("Usando for");
+		
+		for (int i = 0; i < 20; i++) {
+			System.out.println("j:" i++);
+			
+		}
 	}
 
 }
